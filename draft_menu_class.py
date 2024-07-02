a3e1efed749a979a77809b42abd4ccee7533104d
Ether717 <95562180+Ether717@users.noreply.github.com>
2024-07-02 23:11:17 +0800

    fixed print_item

--- draft_menu_class.py
+++ draft_menu_class.py
@@ -8,25 +8,19 @@ class MenuItem:
         self.cost = cost
 
     # Method to print the details of the menu item
-    def print_item(self) -> None:
-        print(f"Name: {self.name}")
-        print(f"Description: {self.description}")
-        print(f"Cost: ${self.cost:.2f}")
+    def print_item(self) -> str:
+        return f"Name: {self.name}\nDescription: {self.description}\nCost: ${self.cost:.2f}\n"
 
- 
 # Create menu items
 pepperoni = MenuItem("Pepperoni Pizza", "Delicious pepperoni pizza", 10.99)
 hawaiian = MenuItem("Hawaiian Pizza", "Sweet and savory Hawaiian pizza", 12.99)
 
-# Create a list of menu items for easier  management and iteration
+# Create a list of menu items for easier management and iteration
 pizza_menu = [pepperoni, hawaiian]
 
 if __name__ == "__main__":
     # Print each menu item's details
     print()
-    
+
     for item in pizza_menu:
-        item.print_item()
-        print()
-        
-        
+        print(item.print_item())
--- draft_process_class.py
+++ draft_process_class.py
@@ -1,21 +1,21 @@
 
 # either importing as a list containing the x amount of menu items
-from draftMenuclass import pizza_menu
+from draft_menu_class import pizza_menu   
 
 def calc_item_cost_list(menu_item, menu_item_index) -> float:
     chosen_item = menu_item[menu_item_index]
-    return chosen_item.cost
+    return chosen_item.cost 
 
 print(calc_item_cost_list(pizza_menu, 1))
 
 
 # or importing each menu item manually 
-from draftMenuclass import pepperoni, hawaiian
+from draft_menu_class import pepperoni, hawaiian 
 
 
 def calc_item_cost_manual(menu_item) -> float:  
     return menu_item.cost
 
-print(calc_item_cost_manual(hawaiian))
+print(calc_item_cost_manual(hawaiian))  
 
 
