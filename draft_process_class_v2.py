2cc5fcd0766c46373555fe554dd248305e0f7870
Ether717 <95562180+Ether717@users.noreply.github.com>
2024-07-04 13:51:54 +0800

    move Assertion tests a stand alone test file

--- draft_process_class_v2.py
+++ draft_process_class_v2.py
@@ -2,48 +2,52 @@ from draft_menu_class import pizza_menu
 
 current_total = 0  # Initialize current_total to 0
 
-
+ 
 class ProcessMenu:
     
     """Converts a menu item object to a dictionary."""
 
-    def __init__(self, menu_items):
+    def __init__(self, menu_items: list):
         self.menu_items = menu_items
         self.item_dict = {}
 
 
-    def get_item_price(self, menu_item_index) -> float:
+    def get_item_price(self, menu_item_index: int) -> float:
         chosen_item = self.menu_items[menu_item_index]
         return chosen_item.cost
 
 
-    def get_item_name(self, menu_item_index) -> str:
+    def get_item_name(self, menu_item_index: int) -> str:
         chosen_item = self.menu_items[menu_item_index]
         return chosen_item.name
 
 
-    def add_to_item_dictionary(self, name, cost) -> dict:
+    def add_to_item_dictionary(self, name: str, cost: float) -> dict:
         self.item_dict[name] = cost
         return self.item_dict
 
 
     def print_item_dictionary(self, dictionary):
-        """Prints the key and values for the item_dict."""
+        """ Prints the key and values for the item_dict. """
         for key, value in dictionary.items():
             print(f"{key}: {value}")
 
 
     def calculate_total_cost(self, dictionary, current_total) -> float:
-        """Calculates the total cost of all items in the dictionary."""
+        """ Calculates the total cost of all items in the dictionary. """
         for value in dictionary.values():
             current_total += value
         return current_total
+
+    def clear_item_dictionary(self):
+        """ Clears the item dictionary. """
+        self.item_dict.clear()
 
 
 if __name__ == "__main__":
 
     def test_process_menu():
-        print("\nTesting ProcessMenu class...")
+        print("\nTesting ProcessMenu class Outcome:")
         
         # Create an instance of the ProcessMenu class 
         process_menu = ProcessMenu(pizza_menu)
@@ -57,26 +61,9 @@ if __name__ == "__main__":
         name = process_menu.get_item_name(1)
         price =process_menu.get_item_price(1)
         process_menu.add_to_item_dictionary(name, price)
-        
-        # Test the get_item_price and get_item_name methods
-        try: 
-            assert process_menu.get_item_price(0) == 10.99
-            assert process_menu.get_item_name(0) == "Pepperoni Pizza"
-            assert process_menu.item_dict == {"Pepperoni Pizza": 10.99}
-        except AssertionError:
-            print("Test failed")
-        
-        # Print the item dictionary
         process_menu.print_item_dictionary(process_menu.item_dict)
         
-       
         # Calculate and print the total cost
         print(f"current_total ${process_menu.calculate_total_cost(process_menu.item_dict, current_total):.2f}")
-
-        # Test the calculate_total_cost method
-        try:
-            assert process_menu.calculate_total_cost(process_menu.item_dict, current_total) == 23.98
-        except AssertionError:
-            print("Test failed")
             
     test_process_menu()
---
+++ test_process_class.py
@@ -0,0 +1,65 @@
+
+from draft_menu_class import pizza_menu   
+from draft_process_class_v2 import ProcessMenu
+
+def test_process_menu(process_menu):
+    
+        print("\nTesting ProcessMenu class...")
+        
+        # populate item dict
+        name = process_menu.get_item_name(0)
+        price = process_menu.get_item_price(0)
+        process_menu.add_to_item_dictionary(name, price)
+
+        # Test the get_item_price and get_item_name methods
+        try: 
+            assert process_menu.get_item_price(0) == 10.99
+            assert process_menu.get_item_name(0) == "Pepperoni Pizza"
+        except AssertionError:
+            print("Test failed for get_item_price and get_item_name methods")
+        else:
+            print("Test passed for get_item_price and get_item_name methods")
+        
+        name = process_menu.get_item_name(1)
+        price = process_menu.get_item_price(1)
+        process_menu.add_to_item_dictionary(name, price)
+
+        # Test the add_to_item_dictionary method
+        try:
+            # index 0 = Pepperoni Pizza: 10.99
+            # index 1 = Hawaiian Pizza: 12.99
+            
+            # process_menu.print_item_dictionary(process_menu.item_dict) # used to check the dictionary
+            
+            assert process_menu.item_dict == {'Pepperoni Pizza': 10.99, 'Hawaiian Pizza': 12.99}
+        except Exception as e:
+            print(f"Test failed for print_item_dictionary method: {e}")
+        else:
+            print("Test passed for print_item_dictionary method")
+        
+        # test the calculate_total_cost method
+        try:
+            current_total = 0
+            new_current_total = process_menu.calculate_total_cost(process_menu.item_dict, current_total)
+            assert new_current_total == 23.98
+        except Exception as e:
+            print(f"Test failed for calculate_total method: {e}")
+        else:
+            print("Test passed for calculate_total method")
+        
+        # teardown
+        process_menu.clear_item_dictionary() # clear the dictionary for the next test
+        
+        print("All tests passed for ProcessMenu class.\n")
+        
+        
+# parsing the pizza_menu list to create a ProcessMenu object
+process_menu = ProcessMenu(pizza_menu)
+
+
+# test the ProcessMenu class
+try: 
+    test_process_menu(process_menu)
+except Exception as e:
+    print(f"test failed for process_menu error at: {e}")
+    
