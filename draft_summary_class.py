d52ef8c266134f55367041fbb363685c04f405e1
Ether717 <95562180+Ether717@users.noreply.github.com>
2024-07-09 16:04:02 +0800

    added main and summary class

---
+++ draft_summary_class.py
@@ -0,0 +1,13 @@
+from draft_menu_class import menu_list
+
+# providing a daily summary of the quantities sold for each pizza variety
+
+class Summary():
+    """" """
+    
+    # constructor
+    def __init__(self,menu_list, order_receipt_list):
+        
+        # both are lists with objects inside to call methods
+        self.menu_list = menu_list
+        self.order_receipt_list = order_receipt_list
