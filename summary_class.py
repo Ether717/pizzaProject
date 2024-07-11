95eb6a6938a2d17d09425964dc1e25c984973349
Ether717 <95562180+Ether717@users.noreply.github.com>
2024-07-11 12:52:21 +0800

    Created new constructors and methods

--- summary_class.py
+++ summary_class.py
@@ -1,13 +1,23 @@
-from menu_class import menu_list
+from menu_class import menu_list as menu
+
 
 # providing a daily summary of the quantities sold for each pizza variety
 
 class Summary():
     """ """
-    
+
     # constructor
-    def __init__(self,menu_list, order_receipt_list):
+    def __init__(self, order_receipt_list, menu_list:list):
+        # order_receipt_list is a list with objects inside to call methods
         
-        # both are lists with objects inside to call methods
-        self.menu_list = menu_list
+        # order_receipt_list = [{name:[price,quantity]},...]
         self.order_receipt_list = order_receipt_list
+        
+        # menu_list = [item,...]
+        self.menu_list = menu_list
+
+    # rest of the class methods...
+
+
+    
+# summary = Summary(order_receipt_list, menu)
---
+++ ui_class.py
@@ -0,0 +1,10 @@
+
+
+
+class UiMenu():
+    """ """
+    
+    def __init__(self) -> None:
+        pass
+        
+        
