86826893ec1a2eb3614ba587cbd76c6449dc3a73
Ether717 <95562180+Ether717@users.noreply.github.com>
2024-07-12 14:05:03 +0800

    added imports to main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from ui_class import UiMenu
+from menu_class import MenuItem
+from menu_class import menu_list
+from process_order_class import ProcessOrder
 
+
+
+def main():
+    process_menu = ProcessOrder(menu_list)
+    ui_menu = UiMenu(menu_list)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
